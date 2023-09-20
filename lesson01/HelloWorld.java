d0ffd111ba9964b6486a061a25d77b5af4f393cc
Luís Falcão <lfalcao@cc.isel.ipl.pt>
2023-09-20 09:50:26 +0100

    Lesson 01: JavaScript introduction

---
+++ lesson01/HelloWorld.java
@@ -0,0 +1,23 @@
+class A {
+    public int x;
+    public A(int x) {
+        this.x = x;
+    }
+}
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println(
+                "Olá Glorioso mundo onde o Maior clube do mundo existe e tem mais títulos que todos os outros");
+
+        A a = new A(5);
+
+        a.x = 10;
+
+        System.out.println(a.x);
+
+        System.out.println = "SLB";
+    }
+}
---
+++ lesson01/hello-world.js
@@ -0,0 +1,24 @@
+console.log("Olá Glorioso mundo onde o Maior clube do mundo existe e tem mais títulos que todos os outros")
+
+class A {
+    constructor(x) {
+        this.x = x
+    }
+}
+
+const a = new A(5)
+
+console.log(a.x)
+
+let f = console.log
+
+
+console.log = "SLB";
+
+
+// //console.log = "SLB"
+
+// console.log(a)
+
+// const b = 123
+
