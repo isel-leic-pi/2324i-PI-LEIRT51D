ab02e66ecc321d1086b26615175579e0661f5aff
Luís Falcão <lfalcao@cc.isel.ipl.pt>
2023-11-08 09:50:47 +0000

    Lesson 15: Tasks application with authentication using Bearer Token

--- lesson15/tasks-server.mjs
+++ lesson15/tasks-server.mjs
@@ -2,6 +2,7 @@
 import cors from 'cors'
 import express from 'express'
 import * as tasksApi from './tasks-web-api.mjs'
+import * as usersApi from './users-web-api.mjs'
 
 const PORT = 1904
 
@@ -14,7 +15,6 @@ let app = express()
 
 
 app.use(cors())
-
 app.use(express.json())
 
 // Get All Tasks: GET /tasks
@@ -31,6 +31,11 @@ app.put('/tasks/:id', tasksApi.updateTask)
 
 // Delete Task: DELETE /tasks/:id
 app.delete('/tasks/:id', tasksApi.deleteTask)
+
+
+// Create User: POST /users
+app.post('/users', usersApi.insertUser)
+
 
 app.listen(PORT, () => console.log(`Server listening in http://localhost:${PORT}`))
 
